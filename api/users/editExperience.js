3a6962f867366c82e2650fac40d1715864b3b61e
Oytun <oytuntutuncu86@gmail.com>
2019-10-21 19:12:37 +0300

    add - edit experience

---
+++ api/users/editExperience.js
@@ -0,0 +1,24 @@
+import { User } from '../../models'
+
+async function action(req, res) {
+  const { name, surname, title, company } = req.body
+
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name,
+      surname,
+      title,
+      company
+    }
+  )
+  res.status(200).json({ user })
+  await user.save()
+}
+
+const editExperience = {
+  method: 'put',
+  action
+}
+
+export default editExperience
